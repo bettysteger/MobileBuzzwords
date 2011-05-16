0584a3ac73a297b63decf2aa022c99ec4ad9e0b6
Bettina Steger <bettina_steger@gmx.at>
2011-05-16 17:48:02 +0200

    starting offline cache and localstorage

--- buzzword.js
+++ buzzword.js
@@ -2,47 +2,18 @@ $(document).bind("ready", function(){
   
   var wl = window.location;
   if (wl.hash) {
-    wl.href = wl.protocol + wl.port + '//' + wl.host + wl.pathname;
+    wl.href = wl.protocol + '//' + wl.host + (wl.port ? ':' + wl.port : '') + wl.pathname;
   }
+
+if (supports_html5_storage()) {
+  if (!localStorage["buzzwords"]) {
+    console.log(get_buzzwords());
+    //init(get_buzzwords);
+  }
+} else {
+  get_buzzwords();
+}
   
-  $.ajax({
-    url: "http://buzzwords.tladesignz.com/data.pl",
-    dataType: "jsonp",
-    success: function(json) {
-      var cat, page, i, html, tmp, bingoWords, allBingoWords;
-      var list = $("#list");
-      
-      for(cat in json) {
-        list.append('<li><a href="#'+cat+'-page">'+cat+'</a></li>');
-        
-        page = $($("#page_template").html());
-        page.attr("id", cat+"-page");
-        page.attr("data-url", cat+"-page");
-        
-        // 5 Zufällige Wörter der Kategorie auswählen
-        allBingoWords = json[cat];
-        bingoWords = [];
-        for(i=0;i<5;i++) {
-          do {
-            var index = Math.round(Math.random()*(allBingoWords.length-1));
-            var word = allBingoWords[index];
-          } while(bingoWords.indexOf(word) !== -1)
-          bingoWords.push(word);
-        }
-        
-        html = "";
-        for(i = 0;i<bingoWords.length;i++) {
-          tmp = bingoWords[i]+"-"+cat;
-          html += '<input type="checkbox" name="checkbox-'+i+'" id="checkbox-'+i+'" class="custom" /><label for="checkbox-'+i+'">'+bingoWords[i]+'</label>';
-        }
-        page.find("h1").text(cat);
-        page.find("fieldset[data-role=controlgroup]").html(html);
-        page = $("body").append(page);
-      }
-      
-      $('#list').listview('refresh');
-    }
-  });
   
   $("input[type=checkbox]").live('change', function(ev) {
     var fieldset = $(ev.currentTarget).closest("fieldset");
@@ -54,3 +25,55 @@ $(document).bind("ready", function(){
     }
   });
 });
+
+function init(json) {
+  var cat, page, i, html, tmp, bingoWords, allBingoWords;
+  var list = $("#list");
+  
+  for(cat in json) {
+    list.append('<li><a href="#'+cat+'-page">'+cat+'</a></li>');
+    
+    page = $($("#page_template").html()); //clone!
+    page.attr("id", cat+"-page");
+    page.attr("data-url", cat+"-page");
+    
+    // 5 Zufällige Wörter der Kategorie auswählen
+    allBingoWords = json[cat];
+    bingoWords = [];
+    for(i=0;i<5;i++) {
+      do {
+        var index = Math.round(Math.random()*(allBingoWords.length-1));
+        var word = allBingoWords[index];
+      } while(bingoWords.indexOf(word) !== -1)
+      bingoWords.push(word);
+    }
+    
+    html = "";
+    for(i = 0;i<bingoWords.length;i++) {
+      tmp = bingoWords[i]+"-"+cat;
+      html += '<input type="checkbox" name="checkbox-'+i+'" id="checkbox-'+i+'" class="custom" /><label for="checkbox-'+i+'">'+bingoWords[i]+'</label>';
+    }
+    page.find("h1").text(cat);
+    page.find("fieldset[data-role=controlgroup]").html(html);
+    page = $("body").append(page);
+  }
+  
+  $('#list').listview('refresh');
+}
+
+function supports_html5_storage() {
+  try {
+    return 'localStorage' in window && window['localStorage'] !== null;
+  } catch (e) {
+    return false;
+  }
+}
+
+function get_buzzwords() {
+  var buzzwords;
+  $.ajax({
+    url: "http://buzzwords.tladesignz.com/data.pl",
+    dataType: "jsonp",
+    success: init
+  });
+}
